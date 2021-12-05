db23dac1a33022dc9c8431e0407eb5cf99b29e42
Abubakir <uzbekovabubakir@gmail.com>
2021-12-05 16:51:46 +0300

    complete HW42

---
+++ src/Components/Users/DelOverlay.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from '../UI/ErorrModal.module.css'
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+
+const DelOverlay = props => {
+    return(
+        <React.Fragment>
+        <Card className={classes.modal}>
+                <header className={classes.header}>
+                    <h2>you definitely want to delete?</h2>
+                </header>
+                <div className={classes.conent}>
+                </div>
+                <footer className={classes.actions}> 
+                    <Button onClick={props.onDelete}>Confirm</Button>
+                    <Button onClick={props.onConfirm}>Cancel</Button>
+                </footer>
+            </Card>
+        </React.Fragment>
+        )
+}
+export default DelOverlay
--- src/Components/Users/DeleteOverlay.js
+++ src/Components/Users/DeleteOverlay.js
@@ -1,21 +1,22 @@
 import React from "react";
-import classes from '../UI/ErorrModal.module.css'
-import Card from "../UI/Card";
-import Button from "../UI/Button";
-    
+import Backdrop from '../UI/Backdrop'
+import ReactDOM from "react-dom";
+import DelOverlay from "./DelOverlay";
+
 const DeleteOverlay = props => {
     return(
-        <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>you definitely want to delete?</h2>
-                </header>
-                <div className={classes.conent}>
-                </div>
-                <footer className={classes.actions}> 
-                    <Button onClick={props.onDelete}>Confirm</Button>
-                    <Button onClick={props.onConfirm}>Cancel</Button>
-                </footer>
-            </Card>
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onConfirm={props.onConfirm}/>,
+                document.getElementById('modal-root')    
+            )}
+            {
+                ReactDOM.createPortal(
+                    <DelOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} onDelete={props.onDelete}/>,
+                    document.getElementById('dan-root')
+                )
+            }
+        </React.Fragment>
         )
 }
 
